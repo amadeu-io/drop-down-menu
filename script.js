7efdc87f690aeb92a3987a67c582a4ff80fb5be8
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-05-03 17:53:17 -0500

    Add toggle visibility functionality of the DDM
    A smooth transition effect has also been added for a nicer UX

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// toggle class visible, that shows & hides the drop down menu
+function toggleDropdownMenu() {
+  dropDownMenu.classList.toggle("visible");
+}
 
+const button = document.querySelector("button");
+const dropDownMenu = document.querySelector(".drop-down-menu");
+
+button.addEventListener("click", toggleDropdownMenu);
